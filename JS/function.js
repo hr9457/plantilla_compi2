f3d408c88c8600ce6d6d1043ce7e12b477484f6d
hector <hr9457@gmail.com>
2021-12-03 12:01:04 -0600

    agregacion de modulos

--- JS/function.js
+++ JS/function.js
@@ -1,16 +1,24 @@
+import {PI,saludo} from './libreria.js';
+
 
 window.addEventListener('load',()=>{
     
+    
     console.log('DOM cargado');
+    console.log(PI);
+    saludo();
+
 
     var contenido = document.getElementById('txtArea1');
+    var areaResultado = document.getElementById('txtArea2');
 
     document.getElementById('btnTraslate').addEventListener('click',accion);
 
     function accion(e) {
-        console.log('saludo');
-        calculator.parse(contenido.value);
+        // console.log('saludo');
+        let resultado = calculator.parse(contenido.value);
+        areaResultado.innerHTML = resultado;
+        saludo();
     }
-
 
 });
---
+++ JS/libreria.js
@@ -0,0 +1,5 @@
+export const PI = 3.1416;
+
+export function saludo(){
+    console.log('hello world');
+};
